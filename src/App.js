350bb05e1ef95ae628d1b4868d1995f6559e5e98
runaiv <aruncoum@gmail.com>
2021-01-05 22:39:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { facebookLoginButton } from 'react-social-login'
+import React, { useState, request } from 'react'
+import LoginForm from '../src/componenets/LoginForm'
+import SignupForm from '../src/componenets/SignupForm'
+import axios from 'axios'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
+
+  const [user, setUser] = useState({name: "", email: ""})
+  const [error, setError] = useState("")
+
+  const Login = async (details) => {
+    console.log(details)
+    
+    const request = {
+      email: details.email,
+      password: details.password
+    }
+
+    const response = await axios.post('http://localhost:3001/auth/login', request)
+    .then( res => {
+      if(res.status !== 200)
+        console.log('incorrect credentials')
+        setUser({
+          name: details.name,
+          email: details.email
+        })
+    }).catch(err => {
+      console.log(err)
+      setError('Wrong Credentials')
+      console.log('Wrong Credentials')
+    })
+  }
+
+const Logout = () => {
+  setUser({name: "", email: ""})
+}
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className = "welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick= {Logout}>Logout</button>
+        </div> 
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )
+    }
     </div>
+    <Route path= "/" component={SignupForm}  />
+    </Router>
   );
 }
 
---
+++ src/componenets/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useReducer, useState } from 'react'
+//import { Button, Alert, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SignupForm from './SignupForm'
+
+
+
+export default function Login({ Login, error}) {
+
+    const [ details, setDetails ] = useState({name: "", email:"", password:""})
+
+    const submitHandler = e => {
+        e.preventDefault()
+
+        Login(details)
+    }   
+    
+    function handleClick(e) {
+        e.preventDefault();
+        console.log('Le lien a été cliqué.');
+      }
+    
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h2>Login</h2>
+                {(error != null) ? ( <div className="error">{error}</div>) : ""}
+
+                <div className="form-group">
+                    <label for="email">Email :</label>
+                    <input type="email" class="form-control" name="email" id="email" onChange = {
+                        e => setDetails({
+                            ...details,
+                            email: e.target.value
+                        }
+                        )
+                    }
+                    value={details.email}/>
+                </div>
+
+                <div className="form-group">
+                    <label for="password">Password :</label>
+                    <input type="password" class="form-control" name="password" id="password" onChange = {
+                        e => setDetails({
+                            ...details,
+                            password: e.target.value
+                        }
+                        )
+                    }
+                    value={details.password}/>
+                </div>
+                <button type="submit" class="btn btn-dark">login</button>
+            </div>
+            <br />
+            <div>
+                <button type="submit" class="btn btn-dark" onClick={handleClick}>Signup</button>
+            </div>
+        </form>
+    )
+}
